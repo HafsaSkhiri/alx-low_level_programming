68842f80049113a7e63a2f79b7d50d25316cc742
Hafsa_Skhiri <HafsaSkhiri08@gmail.com>
2023-03-27 16:27:30 -0700

    .

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,9 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
+
 /**
-  * main - program that generates random valid
-  * passwords for the program 101-crackme
-  * Return: always 0 (Success)
-  */
+ * main - program that generates random valid
+ * passwords for the program 101-crackme
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int pass[100];
@@ -26,5 +30,6 @@ int main(void)
 			break;
 		}
 	}
+
 	return (0);
 }
